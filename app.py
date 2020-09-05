292acf4b00345b863dd22047b57df9152450aea3
rahul chugh <rahulchugh71995@gmail.com>
2020-09-05 16:14:10 +0530

    changed date transformer to be taken from file and  model will be built from that

--- app.py
+++ app.py
@@ -4,40 +4,8 @@ import joblib
 import pandas as pd
 import os
 import numpy as np
-#from date_transformer import DateTransformer
+from date_transformer import DateTransformer
 
-
-
-
-from sklearn.base import BaseEstimator,TransformerMixin
-import time
-
-class DateTransformer(BaseEstimator,TransformerMixin):
-    def __init__(self):
-        pass
-    def fit(self,X,y=None):
-        return self
-    def transform(self,X,y=None):
-        newx = X.copy()
-        date_attribs = ['Date_of_Journey','Dep_Time','Arrival_Time','Duration']
-        newx['Date_of_Journey'] = pd.to_datetime(newx['Date_of_Journey'])
-        newx['Date_of_Journey'+'_year'] = time.localtime().tm_year - newx['Date_of_Journey'].dt.year
-        newx['Date_of_Journey'+'_month'] = newx['Date_of_Journey'].dt.month
-        newx['Date_of_Journey'+'_day'] = newx['Date_of_Journey'].dt.day
-        
-        newx['Dep_Time'] = pd.to_datetime(newx['Dep_Time'])
-        newx['Dep_Time'+'_hour'] = newx['Dep_Time'].dt.hour
-        newx['Dep_Time'+'_minute'] = newx['Dep_Time'].dt.minute
-        
-        newx['Arrival_Time'] = pd.to_datetime(newx['Arrival_Time'])
-        newx['Arrival_Time'+'_hour'] = newx['Arrival_Time'].dt.hour
-        newx['Arrival_Time'+'_minute'] = newx['Arrival_Time'].dt.minute
-        
-        newx['Duration_hour'] = newx['Duration'].str.extract(r'(\d+)h').fillna(0)
-        newx['Duration_min'] = newx['Duration'].str.extract(r'(\d+)m').fillna(0)
-        
-        return newx.drop(date_attribs,axis=1)
-        
 app = Flask(__name__)
 model_folder = "model"
 model_name = "best_model.pkl"
